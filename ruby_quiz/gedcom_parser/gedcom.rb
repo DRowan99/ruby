14e9d2c314d085db6e42de3863d87b19a5d6fbd0
Dave Rowan <drowan99@gmail.com>
2016-03-20 23:56:17 -0400

    Updates to GEDCOM parser. Pretty functional, nested output needs to be indented appropriately still.
    Signed-off-by: Dave Rowan <drowan99@gmail.com>

--- ruby_quiz/gedcom_parser/gedcom.rb
+++ ruby_quiz/gedcom_parser/gedcom.rb
@@ -1,33 +1,55 @@
 module GEDCOM
-	require 'builder'
-
 	GEDCOM_DIR = File.expand_path(File.dirname(__FILE__))
 
 	class Parser
-		attr_reader :current_depth, :current_line, :xml
+		attr_reader :current_depth, :depth, :open_tags, :xml
 
 		DEFAULT_FILE = '/us_presidents.ged'
 
 		def initialize(gedcom_file = GEDCOM::GEDCOM_DIR+DEFAULT_FILE)
-			@xml = Builder::XmlMarkup.new(:indent => 2)
+			@xml = ""
 			@gedcom_file = File.open(gedcom_file)
+			@open_tags, @depth = [], []
 		end
 
 		def parse!()
-			line = @gedcom_file.readline
+			loop do 
+				ged_parse @gedcom_file.readline
+			end until @gedcom_file.eof?
 		end
 
-		def parse_line(ged_str)
-			ged = ged_str.split(" ")
-			@current_depth = ged.shift
+		def ged_parse(ged_line)
+			ged = ged_line.split(" ")
+			@current_depth = ged.shift.to_i
 
-			case @current_depth
-			when "0"
-				@id, @current_tag, @content = ged
-			else
-				@current_tag, *@content = ged
+			while @depth.last && @current_depth <= @depth.last
+				xml_close_open_tags
 			end
-			@xml.__send__(@current_tag, @content ? @content.join(" ") : {id: @id})
+
+			id = (@current_depth == 0 ? ged.shift : nil)
+
+			tag, *content = ged
+			xml_open(tag, id: id, text: content.join(" "))
+
+			@xml
+		end
+
+		private
+
+		def xml_open(tag, options = {})
+			@xml << "<#{tag}"
+			@xml << " id=\"#{options[:id]}\"" if options[:id]
+			@xml << ">#{options[:text]}"
+			@open_tags << tag
+			@depth << @current_depth
+		end
+
+		def xml_close_open_tags
+			unless @current_depth == @depth.pop
+				@xml << "\n"
+				@xml << @current_depth * 2 * " " 
+			end
+			@xml << "</#{@open_tags.pop}>"
 		end
 	end
 end
